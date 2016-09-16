f6baf331f706b95fb9da8e108a8bd062b31197e9
pavelmakarets <makaretsp@mail.ru>
2016-09-16 16:55:28 +0300

    version 0.0.2

--- Word.h
+++ Word.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Textprocess.h"
+#include <string>
 
 class Word:public Textprocess
 {
@@ -8,5 +9,50 @@ public:
 	~Word();
 private:
 	Developer developer;
+
+	/*enum Fonts
+	{
+		Arial,
+		Calibri,
+		Times_New_Roman,
+		Lucida_Console
+	};*/
+
+	class Font
+	{
+		char fonts[4][16] = {
+			"Arial",
+			"Calibri",
+			"Times New Roman",
+			"Lucida Console"
+		};
+		std::string font;
+		int size;
+	public:
+		std::string getfont()
+		{
+			return this->font;
+		}
+		void setfont(std::string font)
+		{
+			for (int i = 0; i < 4; i++)
+			{
+				if (fonts[i] == font)
+				{
+					this->font = font;
+				}
+			}
+		}
+		int getsize()
+		{
+			return this->size;
+		}
+		void setsize(int size)
+		{
+			this->size = size;
+		}
+	};
+
+	Font font;
 };
 
